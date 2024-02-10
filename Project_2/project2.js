66b354f834c84ef25d96ad3beb586a06e1977eba
AnkitRana9536 <158178275+AnkitRana9536@users.noreply.github.com>
2024-02-10 13:05:30 +0000

    Project2

--- Project_2/project2.js
+++ Project_2/project2.js
@@ -5,12 +5,25 @@ form.addEventListener('submit', function (e) {
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const result = document.querySelector('#result')
+    const msg = document.querySelector('#msg')
     if (height === '' || height < 0 || isNaN(height)) {
         result.innerHTML = `Please give a valid height ${height}`;
-    }    else if (weight === '' || weight < 0 || isNaN(weight)) {
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
         result.innerHTML = `Please give a valid weight ${weight}`;
-    }else{
-       const bmi = (weight/((height*height)/1000)).toFixed(2)
-       result.innerHTML = `<span>${bmi}</span>`
+    } else {
+        const bmi = (weight / ((height * height) / 1000)).toFixed(2)
+        result.innerHTML = `<span>${bmi}</span>`
+    }
+    console.log(result.textContent, 'result')
+
+    if (result.textContent < 18.6) {
+        console.log(result, 'result')
+        msg.innerHTML = `This is under Weight`
+    } else if (result.textContent >= 18.6 && result.textContent <= 24.9) {
+        msg.innerHTML = `This is Normal Weight`
+
+    }else if (result.textContent > 24.9) {
+        msg.innerHTML = `This is Over Weight`
+
     }
 });
